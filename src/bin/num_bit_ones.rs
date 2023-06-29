a9807eb152cb17ec4658134e729656c7861a9c55
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-06-29 11:01:22 +0200

    Number of 1 Bit

---
+++ src/bin/num_bit_ones.rs
@@ -0,0 +1,22 @@
+pub fn main() {
+    println!("Number of 1 Bits");
+}
+
+// O(1)
+pub fn hamming_weight(n: u32) -> i32 {
+    n.count_ones() as i32
+}
+
+// O(1)
+pub fn hamming_weight_man(n: u32) -> i32 {
+    let mut res: i32 = 0;
+
+    for i in 0..32 {
+        let mask = 1 << i;
+        if n & mask != 0 {
+            res += 1;
+        }
+    }
+
+    res
+}
