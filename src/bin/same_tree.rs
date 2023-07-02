60533fc41eb01807240991c69516ddb7c63b7bbb
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-07-02 12:10:42 +0200

    Same Tree

---
+++ src/bin/same_tree.rs
@@ -0,0 +1,39 @@
+pub fn main() {
+    println!("Same Tree");
+}
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+struct Solution;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    // O(n) Time: O(logn)
+    pub fn is_same_tree(
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> bool {
+        match (p, q) {
+            (Some(p_node), Some(q_node)) => {
+                p_node.borrow().val == q_node.borrow().val
+                    && Self::is_same_tree(
+                        p_node.borrow().left.clone(),
+                        q_node.borrow().left.clone(),
+                    )
+                    && Self::is_same_tree(
+                        p_node.borrow().right.clone(),
+                        q_node.borrow().right.clone(),
+                    )
+            }
+            (None, None) => true,
+            _ => false,
+        }
+    }
+}
