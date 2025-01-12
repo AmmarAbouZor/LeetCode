ab6569f26bf32560bf8c02c55508f780f6d219c8
Ammar Abou Zor <ammarabouzor@gmail.com>
2025-01-12 18:08:08 +0100

    Roman to int: Modelling solution.

--- src/bin/roman_to_int.rs
+++ src/bin/roman_to_int.rs
@@ -37,3 +37,76 @@ pub fn roman_to_int(s: String) -> i32 {
         }
     })
 }
+
+// This solution is simple and direct. It has modelling a lot and doesn't have tweaks
+// Time: O(n)
+mod final_soltion {
+    use std::char;
+
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    enum Roman {
+        I,
+        V,
+        X,
+        L,
+        C,
+        D,
+        M,
+    }
+
+    impl Roman {
+        #[inline]
+        fn num(self) -> i32 {
+            match self {
+                Roman::I => 1,
+                Roman::V => 5,
+                Roman::X => 10,
+                Roman::L => 50,
+                Roman::C => 100,
+                Roman::D => 500,
+                Roman::M => 1000,
+            }
+        }
+
+        fn next_reduce(self) -> &'static [Self] {
+            match self {
+                Roman::I => &[Roman::V, Self::X],
+                Roman::X => &[Roman::L, Roman::C],
+                Roman::C => &[Roman::D, Roman::M],
+                Roman::V | Roman::L | Roman::D | Roman::M => &[],
+            }
+        }
+    }
+
+    impl From<char> for Roman {
+        fn from(value: char) -> Self {
+            match value {
+                'I' => Roman::I,
+                'V' => Roman::V,
+                'X' => Roman::X,
+                'L' => Roman::L,
+                'C' => Roman::C,
+                'D' => Roman::D,
+                'M' => Roman::M,
+                invalid => panic!("Invalid char: {invalid}"),
+            }
+        }
+    }
+
+    pub fn roman_to_int(s: String) -> i32 {
+        let romans: Vec<Roman> = s.chars().map(|ch| ch.into()).collect();
+        let mut sum = 0;
+
+        for i in 0..romans.len() {
+            let current = romans[i];
+            let next = romans.get(i + 1);
+            if next.is_some_and(|n| current.next_reduce().contains(n)) {
+                sum -= current.num();
+            } else {
+                sum += current.num();
+            }
+        }
+
+        sum
+    }
+}
